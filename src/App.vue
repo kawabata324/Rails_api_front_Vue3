c2696b23b8a9d9ba439bdaa0e7568266e2c4e324
kawabata <s1931115@s.konan-u.ac.jp>
2021-12-09 09:51:45 +0900

    axios apiからdataをとってくる

--- src/App.vue
+++ src/App.vue
@@ -1,1 +1,20 @@
+import { defineComponent } from 'vue';
+<template>
+  <div class="app">
+    <div class="">
+      <router-link to= "/about">about</router-link>
+      <router-link to= "/">home</router-link>
+    </div>
+    <router-view></router-view>
+  </div>
+</template>
 
+<script lang = "ts">
+import {defineComponent} from 'vue'
+
+export default defineComponent({
+
+});
+</script>
+
+<style></style>
--- src/views/Home.vue
+++ src/views/Home.vue
@@ -1,18 +1,50 @@
 <template>
   <div class="home">
-    <img alt="Vue logo" src="../assets/logo.png" />
-    <HelloWorld msg="Welcome to Your Vue.js + TypeScript App" />
+    <ul v-for="data in dataList" :key="data.id">
+      <li>
+        <label>id</label>
+        {{ data.id }}
+        <label>name</label>
+        {{ data.name }}
+        <label>email</label>
+        {{ data.email }}
+        <label>age</label>
+        {{ data.age }}
+      </li>
+    </ul>
   </div>
 </template>
 
 <script lang="ts">
 import { defineComponent } from "vue";
-import HelloWorld from "@/components/HelloWorld.vue"; // @ is an alias to /src
+import { reactive } from "@vue/reactivity";
+import { onMounted } from "@vue/runtime-core";
+import axios from "axios";
+
+interface User{
+  id: number;
+  name: string;
+  email: string;
+  age: number;
+  created_at: Date;
+}
 
 export default defineComponent({
   name: "Home",
-  components: {
-    HelloWorld,
+  setup() {
+    let dataList: User[] = reactive([]);
+    const url = "http://localhost:3004/users";
+    onMounted(() => {
+      axios.get(url).then((res) => {
+        console.log(res.data);
+        res.data.forEach((element: User) => {
+          dataList.push(element);
+        });
+      });
+    });
+    return {
+      dataList,
+    };
   },
 });
 </script>
