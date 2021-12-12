043394f66ef00f0c36381a0388f5296168288756
kawabata <s1931115@s.konan-u.ac.jp>
2021-12-12 11:47:36 +0900

    a

---
+++ src/views/Memo.vue
@@ -0,0 +1,23 @@
+<template>
+  <div>aa</div>
+</template>
+
+<script lang="ts">
+import { defineComponent } from "vue";
+import Client from "../api/client";
+import { onMounted } from '@vue/runtime-core';
+
+export default defineComponent({
+  setup() {
+    onMounted(()=>{
+      Client.get("/api/memos").then((res) => {
+        console.log(res);
+      });
+    })
+    return {
+    };
+  },
+});
+</script>
+
+<style></style>
